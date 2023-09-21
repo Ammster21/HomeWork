8653424f3c27439192bca2c5ee7a1b99f3c74aa6
Artem Gorbachev <ammster21@Air-Artem.Dlink>
2023-09-21 22:06:30 +0300

    сделал домашнюю работу №1

--- HomeWork/main.swift
+++ HomeWork/main.swift
@@ -7,5 +7,15 @@
 
 import Foundation
 
-print("Hello, World!")
+let deposit: Double = 1000
+let perсent: Double = 1 + 5/100
+
+let period1: Double = deposit * perсent
+let period2: Double = period1 * perсent
+let period3: Double = period2 * perсent
+let period4: Double = period3 * perсent
+let total: Double = period4 * perсent
+
+
+print(total)
 
