0bffa9da7336847af2cdce0aa8dfffe5c13989d0
Artem Gorbachev <ammster21@Air-Artem.Dlink>
2023-09-26 00:01:57 +0300

    UPD домашняя работа№1

--- HomeWork/main.swift
+++ HomeWork/main.swift
@@ -8,13 +8,15 @@
 import Foundation
 
 let deposit: Double = 1000
-let perсent: Double = 1 + 5/100
+let perсent: Double = 5
+let multiplay = 1 + perсent / 100
 
-let period1: Double = deposit * perсent
-let period2: Double = period1 * perсent
-let period3: Double = period2 * perсent
-let period4: Double = period3 * perсent
-let total: Double = period4 * perсent
+let period1: Double = deposit * multiplay
+let period2: Double = period1 * multiplay
+let period3: Double = period2 * multiplay
+let period4: Double = period3 * multiplay
+let total: Double = period4 * multiplay
+
 
 
 print(total)
